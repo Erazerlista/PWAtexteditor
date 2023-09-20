af34aa37680cd72c815973c8938f9f23a371ef06
Erazerlista <128334161+Erazerlista@users.noreply.github.com>
2023-09-19 17:18:26 -0700

    code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,70 +5,75 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file. done.
 // TODO: Add CSS loaders and babel to webpack. added.
-
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./index.html",
-        title: "JATE",
-      }),
-      new InjectManifest({
-        swSrc: "./src-sw.js",
-        swDest: "src-sw.js",
-      }),
-      // creates a manifest.json file
-      new WebpackPwaManifest({
-        fingerprints: false,
-        inject: true,
-        name: "Just Another Text Editor",
-        short_name: "jate",
-        description: "Text Editor with offline capabilities using IndexedDB",
-        background_color: "#225ca3",
-        theme_color: "#225ca3",
-        start_url: "/",
-        publicPath: "/",
-        icons: [
-          {
-            src: path.resolve("src/images/logo.png"),
-            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
-            destination: path.join("assets", "icons"),
-          },
-        ],
-      }),
-    ],
-
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+    return {
+        mode: 'development',
+        entry: {
+            main: './src/js/index.js',
+            install: './src/js/install.js',
+            database: './src/js/database.js',
+            editor: './src/js/editor.js',
+            header: './src/js/header.js',
         },
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env"],
-              plugins: [
-                "@babel/plugin-proposal-object-rest-spread",
-                "@babel/transform-runtime",
-              ],
-            },
-          },
-        }
-        
-      ],
-    },
-  };
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        plugins: [
+            // added indexedDB
+            // Webpack plugin that generates our html file and injects our bundles.
+            new HtmlWebpackPlugin({
+                template: './index.html',
+                title: 'Webpack Plugin',
+            }),
+            //  added inject
+            // Injects our custom service worker
+            new InjectManifest({
+                swSrc: './src-sw.js',
+                swDest: 'src-sw.js',
+            }),
+            // Creates a manifest.json file.
+            new WebpackPwaManifest({
+                fingerprints: false,
+                inject: true,
+                name: 'Just Another Text Editor',
+                short_name: 'JATE',
+                description: 'Just Another Text Editor',
+                background_color: '#225ca3',
+                theme_color: '#225ca3',
+                start_url: './',
+                publicPath: './',
+                icons: [
+                    {
+                        src: path.resolve('src/images/logo.png'),
+                        sizes: [96, 128, 192, 256, 384, 512],
+                        destination: path.join('assets', 'icons'),
+                    },
+                ],
+            }),
+        ],
+
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                            plugins: [
+                                '@babel/plugin-proposal-object-rest-spread',
+                                '@babel/transform-runtime',
+                            ],
+                        },
+                    },
+                },
+            ],
+        },
+    };
 };
